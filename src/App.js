da5f00fdd6810df29247647bfdaa4a283ea56433
Bhavesh <bhavesh.21910572@viit.ac.in>
2023-08-02 19:05:49 +0530

    made frontend changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
+import axios from 'axios';
+import AddState from './Components/AddState';
+import GetState from './Components/GetState';
 
 function App() {
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AddState />
+      <GetState />
     </div>
   );
 }
---
+++ src/Components/AddState.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+function AddState() {
+  const [stateName, setStateName] = useState();
+  const handleOnChange = (e) => {
+    setStateName(e.target.value);
+  }
+  
+  const handleOnSubmit = () => {
+    const submitData = async () => {
+      const response = await axios.post("http://localhost:8080/addState",{
+        StateNAME: stateName
+      });
+      console.log('===== response - ', response);
+    }
+
+    submitData();
+  }
+  return (
+    <div>
+      <input type="text" onChange={handleOnChange}/>
+      <button type="submit" onClick={handleOnSubmit}>Submit</button>
+    </div>
+  )
+}
+
+export default AddState
